545c7c01edf86a62a1ba416cb7722484bc176156
Dheeraj Yadav <>
2019-10-13 14:38:48 +0530

    update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/sample-issuemanagement-app'));
+app.use(express.static('./dist/frontent'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/sample-issuemanagement-app/index.html'));
+res.sendFile(path.join(__dirname,'/dist/frontent/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
